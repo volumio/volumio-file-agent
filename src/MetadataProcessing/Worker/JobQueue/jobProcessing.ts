4e005fd4d4693bd42e5e6c0d35bcfbe1cd7211f9
Giacomo Triggiano <giacomotriggiano@gmail.com>
2020-04-13 23:46:06 +0200

    Remove checksum calculation

--- src/MetadataProcessing/Worker/JobQueue/jobProcessing.ts
+++ src/MetadataProcessing/Worker/JobQueue/jobProcessing.ts
@@ -1,9 +1,9 @@
 import { AsyncResultIterator } from 'async'
-import { Either, isLeft, left, right } from 'fp-ts/lib/Either'
+import { Either, left, right } from 'fp-ts/lib/Either'
 import * as mm from 'music-metadata'
 import path from 'path'
 
-import { checksum } from '../../../fs'
+// import { checksum } from '../../../fs'
 
 export const jobProcessing: AsyncResultIterator<Job, JobResult, never> = async (
   job,
@@ -16,59 +16,59 @@ export const jobProcessing: AsyncResultIterator<Job, JobResult, never> = async (
 
   const filePath = path.resolve(job.file.folder, job.file.name)
 
-  const checksumResult = await checksum(filePath)
+  // const checksumResult = await checksum(filePath)
 
-  if (isLeft(checksumResult)) {
+  // if (isLeft(checksumResult)) {
+  //   /**
+  //    * The fact that we failed to caculate the checksum
+  //    * means we've got problems reading the file,
+  //    * so we consider the job failed.
+  //    */
+  //   done(
+  //     null,
+  //     left({
+  //       ...jobInfos,
+  //       error: checksumResult.left,
+  //     }),
+  //   )
+  // } else {
+  const fileChecksum = ''
+
+  if (job.file.checksum !== undefined && fileChecksum === job.file.checksum) {
     /**
-     * The fact that we failed to caculate the checksum
-     * means we've got problems reading the file,
-     * so we consider the job failed.
+     * The checksum of the file is equal to the
+     * one known by job requester,
+     * so we do not process the file further.
      */
+    return done(null, right(job as JobSuccess))
+  }
+
+  try {
+    const metadata = await mm.parseFile(filePath, {
+      duration: true,
+    })
+
+    done(
+      null,
+      right({
+        ...jobInfos,
+        file: {
+          ...job.file,
+          checksum: fileChecksum,
+          metadata,
+        },
+      }),
+    )
+  } catch (error) {
     done(
       null,
       left({
         ...jobInfos,
-        error: checksumResult.left,
+        error,
       }),
     )
-  } else {
-    const fileChecksum = checksumResult.right
-
-    if (job.file.checksum !== undefined && fileChecksum === job.file.checksum) {
-      /**
-       * The checksum of the file is equal to the
-       * one known by job requester,
-       * so we do not process the file further.
-       */
-      return done(null, right(job as JobSuccess))
-    }
-
-    try {
-      const metadata = await mm.parseFile(filePath, {
-        duration: true,
-      })
-
-      done(
-        null,
-        right({
-          ...jobInfos,
-          file: {
-            ...job.file,
-            checksum: fileChecksum,
-            metadata,
-          },
-        }),
-      )
-    } catch (error) {
-      done(
-        null,
-        left({
-          ...jobInfos,
-          error,
-        }),
-      )
-    }
   }
+  // }
 }
 
 type JobInfos = {
