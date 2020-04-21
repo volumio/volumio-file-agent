7b75a26d3bcacc5b035ad19342b8b15cf8209700
Giacomo Triggiano <giacomotriggiano@gmail.com>
2020-04-21 13:45:44 +0200

    ...

--- src/moduleAlias.ts
+++ src/moduleAlias.ts
@@ -3,6 +3,7 @@ import path from 'path'
 
 moduleAlias.addAliases({
   '@adapters': path.resolve(__dirname, 'adapters'),
+  '@Agent': path.resolve(__dirname, 'Agent'),
   '@fs': path.resolve(__dirname, 'fs'),
   '@HTTPServer': path.resolve(__dirname, 'HTTPServer'),
   '@ports': path.resolve(__dirname, 'ports'),
