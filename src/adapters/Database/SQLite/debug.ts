c36701f23b99febe54fb8a0936c55b80dd2d8cf2
Giacomo Triggiano <giacomotriggiano@gmail.com>
2020-04-23 12:40:38 +0200

    ...

--- src/adapters/Database/SQLite/debug.ts
+++ src/adapters/Database/SQLite/debug.ts
@@ -1,12 +1,12 @@
 import D from 'debug'
 
-const NAMESPACE = `fa:persistency:sqlite`
+const NAMESPACE = `fa:database:sqlite`
 
 const debug = {
   info: D(NAMESPACE),
   error: D(NAMESPACE),
-  dbQuery: D(`${NAMESPACE}:db-query`),
-  usecase: D(`${NAMESPACE}:usecase-execution`),
+  dbQuery: D(`${NAMESPACE}:query`),
+  usecase: D(`${NAMESPACE}:usecase`),
 }
 
 debug.info.log = console.log.bind(console)
