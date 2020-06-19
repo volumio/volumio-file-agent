79cbd2147b311e34e4758e92ee98bc9f5d9cffb7
Giacomo Triggiano <giacomotriggiano@gmail.com>
2020-06-19 16:11:07 +0200

    Fix moduleAlias

--- src/moduleAlias.ts
+++ src/moduleAlias.ts
@@ -2,8 +2,6 @@ import moduleAlias from 'module-alias'
 import path from 'path'
 
 moduleAlias.addAliases({
-  '@adapters': path.resolve(__dirname, 'adapters'),
   '@Agent': path.resolve(__dirname, 'Agent'),
   '@HTTPServer': path.resolve(__dirname, 'HTTPServer'),
-  '@ports': path.resolve(__dirname, 'ports'),
 })
